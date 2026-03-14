2d6bc18be046737ba46b4fc2bd0be8da912b193c
Aravindhan-loganathan <aravindhanl2004@gmail.com>
2026-03-14 11:37:05 +0530

    user defined properly

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,7 +4,7 @@ const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, enum: ['faculty', 'member', 'student'], required: true },
+  role: { type: String, enum: ['faculty','member','student','coordinator'], required: true },
   clubs: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Club' }]
 });
 
